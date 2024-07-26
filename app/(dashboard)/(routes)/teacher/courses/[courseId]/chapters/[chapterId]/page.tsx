b54fb49af7875f12b57ff9eca47117ae4cc5a406
unknown <hengleap70@gmail.com>
2024-07-26 12:59:19 +0700

    lms

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,8 +1,11 @@
+import { IconBadge } from "@/components/icon-badge";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
-import { ArrowLeft } from "lucide-react";
+import { ArrowLeft, LayoutDashboard } from "lucide-react";
 import Link from "next/link";
 import { redirect } from "next/navigation";
+import ChapterTitleForm from "./_components/chapter-title-form";
+import ChapterDescriptionForm from "./_components/chapter-description-form";
 
 const ChpaterIdPage = async ({
   params,
@@ -54,6 +57,26 @@ const ChpaterIdPage = async ({
                 Complete all field {completionText}
               </span>
             </div>
+          </div>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+        <div className="space-y-4">
+          <div>
+            <div className="flex items-center gap-x-2">
+              <IconBadge icon={LayoutDashboard} />
+              <h2 className="text-xl">Customize your chapter</h2>
+            </div>
+            <ChapterTitleForm 
+              initialData={chapter}
+              courseId={params.courseId}
+              chapterId={params.chapterId}
+            />
+            <ChapterDescriptionForm 
+              initialData={chapter} 
+              courseId={params.courseId}
+              chapterId={params.chapterId}
+            />
           </div>
         </div>
       </div>
---
+++ components/preview.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import dynamic from "next/dynamic";
+import { useMemo } from "react";
+
+import "react-quill/dish/quill.snow.css";
+
+interface PreviewProps {
+  value: string;
+}
+
+export const Preview = ({ value }: PreviewProps) => {
+  const ReactQuill = useMemo(
+    () =>
+      dynamic(() => import("react-quill"), {
+        ssr: false,
+      }),
+    []
+  );
+
+  return (
+    <div className="bg-white">
+        <ReactQuill 
+            theme="bubble"
+            value={value}
+            readOnly
+        />
+    </div>
+  )
+};
