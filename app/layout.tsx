54afa6ae2001aed8a282f01760c6b0c0d8aa8318
unknown <hengleap70@gmail.com>
2024-10-29 16:48:12 +0700

    lms

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
@@ -31,6 +32,7 @@ export default function RootLayout({
           <Toaster />
           <ConvexClientProvider>{children}</ConvexClientProvider>
           <SpeedInsights />
+          <Analytics />
         </body>
       </html>
     </ClerkProvider>
